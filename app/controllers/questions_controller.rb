b35fbc5f5f62005a4a8d0ddce029cb8efb11280b
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-07-22 10:05:09 -0700

    Added seeds and tag associations to Question model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,12 @@ class QuestionsController < ApplicationController
     before_action :find_question, only: [:show, :edit, :update, :destroy]
     before_action :authorize_user!, only: [:edit, :update, :destroy]
 
+    #CRUD ACTIONS: 
+    #1. Create ( #new and #create )
+    #2. Read (#index and #show)
+    #3. Update (#edit and #update)
+    #4. Destroy (#destroy)
+
     def index
         @questions = Question.all.order(created_at: :desc) 
         #Model.all is a method built into active record used to return all records of that model
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
     has_many :taggings, dependent: :destroy
     has_many :questions, through: :taggings
 
-    validates :name, :presence: true, uniqueness: { case_sensitive: false }
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
 
     #The case_sensitive option will make uniqueness validation ignore case. i.e. two records of "Science" and "SCIENCE" can't co-exist
 end
