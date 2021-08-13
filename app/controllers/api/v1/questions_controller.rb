6b6c14d73e3d486384c50c194fc46d326d2f33fd
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-08-13 10:36:55 -0700

    Add validation error handling for questions_controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -9,6 +9,8 @@ class Api::V1::QuestionsController < Api::ApplicationController
     before_action :find_question, only: [:show, :destroy, :update]
     before_action :authenticate_user!, only: [ :create, :destroy, :update ]
 
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
     def index
         #curl -H "Accept: application/json" http://localhost:3000api/v1/questions
         questions = Question.order(created_at: :desc)
@@ -18,36 +20,44 @@ class Api::V1::QuestionsController < Api::ApplicationController
     end
 
     def show
+        if @question
         #curl -H "Accept: application/json" http://localhost:3000/api/v1/questions/:id
         #OR
         #curl http://localhost:3000/api/v1/questions/5
         # question = Question.find(params[:id])
         render(json: @question)
+        else
+            render(json: {error: "Question Not Found"})
+        end
     end
 
     def create
         question = Question.new question_params
         question.user = current_user
 
-        if question.save
-            render json: { id: question.id }
-        else
-            render(
-                json: { errors: question.errors.messages },
-                status: 422 #Unprocessable entity
-            )
-        end
+        # if question.save
+        #     render json: { id: question.id }
+        # else
+        #     render(
+        #         json: { errors: question.errors.messages },
+        #         status: 422 #Unprocessable entity
+        #     )
+        # end
+        question.save!
+        render json: {id: question.id}
     end
 
     def update
-        if @question.update question_params
-            render json: { id: @question.id }
-        else
-            render(
-                json: { errors: @question.errors.messages },
-                status: 422
-            )
-        end
+        # if @question.update question_params
+        #     render json: { id: @question.id }
+        # else
+        #     render(
+        #         json: { errors: @question.errors.messages },
+        #         status: 422
+        #     )
+        # end
+        @question.update question_params
+        render json: { id: @question.id }
     end
 
     def destroy
@@ -67,5 +77,23 @@ class Api::V1::QuestionsController < Api::ApplicationController
     def question_params
         params.require(:question).permit(:title, :body)
     end
+
+    def record_invalid(error)
+        invalid_record = error.record
+        errors = invalid_record.errors.map do |field, message|
+            {
+                type: error.class.to_s, #need it in string format
+                record_type: invalid_record.class.t_s,
+                field: field,
+                message: message
+            }
+        end
+        render(
+            json: {status: 422, errors: errors },
+            status: 422 #alias is :unprocessable_entity
+        )
+    end
+
+
 
 end
